577fa84b2a3b703e60b1fbd051f3d0a8ecbf4a70
Alexander Bezzubov <bzz@apache.org>
2017-09-18 15:12:10 +0200

    UAST: switch bblfsh client to bblfsh/client-scala#1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,8 @@ object Dependencies {
   lazy val newerHadoopClient = "org.apache.hadoop" % "hadoop-client" % "2.7.2"
   lazy val jgit = "org.eclipse.jgit" % "org.eclipse.jgit" % "4.8.0.201706111038-r"
   lazy val siva = "com.github.src-d" % "siva-java" % "master-SNAPSHOT"
-  lazy val bblfsh = "com.github.bblfsh" % "client-scala" % "master-SNAPSHOT"
+  lazy val bblfsh = "com.github.bzz" % "client-scala" % "update-protobuff-SNAPSHOT"
+  // untill https://github.com/bblfsh/client-scala/pull/1 is merged
+  //lazy val bblfsh = "com.github.bblfsh" % "client-scala" % "master-SNAPSHOT"
   lazy val commonsIO = "commons-io" % "commons-io" % "2.5"
 }
