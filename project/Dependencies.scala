a7b2ff4113928042253c7e6e114f8c73161cf4b3
Alexander Bezzubov <bzz@apache.org>
2017-09-20 10:48:33 +0200

    UAST: bump bblfsh/scala-client version to v0.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   lazy val fixNettyForGrpc = "io.netty" % "netty-all" % "4.1.11.Final"
   lazy val jgit = "org.eclipse.jgit" % "org.eclipse.jgit" % "4.8.0.201706111038-r"
   lazy val siva = "com.github.src-d" % "siva-java" % "master-SNAPSHOT"
-  lazy val bblfsh = "com.github.bblfsh" % "client-scala" % "master-SNAPSHOT"
+  lazy val bblfsh = "com.github.bblfsh" % "client-scala" % "v0.1.0"
   lazy val enry = "tech.sourced" % "enry-java" % "1.0"
   lazy val commonsIO = "commons-io" % "commons-io" % "2.5"
 }
