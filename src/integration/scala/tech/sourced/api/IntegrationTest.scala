05fcb02d21584d96adff3615c7ca1d5adaa7f165
Miguel Molina <miguel@erizocosmi.co>
2017-10-04 10:34:41 +0200

    tests: move away from sql file to be able to check results in integration tests
    Signed-off-by: Miguel Molina <miguel@erizocosmi.co>

--- src/integration/scala/tech/sourced/api/IntegrationTest.scala
+++ src/integration/scala/tech/sourced/api/IntegrationTest.scala
@@ -1,24 +1,44 @@
 package tech.sourced.api
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
 
-import scala.io.Source
-
 class IntegrationTest extends WordSpec with Matchers with BeforeAndAfterAll {
+
+  type DataFrameCheck = (DataFrame) => Unit
 
   var ss: SparkSession = _
   val resourcesPath = getClass.getResource("/siva-files").toString
 
+  val tests = Map[String, (Int, DataFrameCheck)](
+    "SELECT * FROM repositories WHERE is_fork = false" -> (4, noop _),
+    "SELECT * FROM _references WHERE name='refs/heads/master'" -> (5, noop _),
+    "SELECT * FROM commits WHERE hash='fff7062de8474d10a67d417ccea87ba6f58ca81d'" -> (43, noop _),
+    "SELECT * FROM files WHERE path='README.md'" -> (2039, noop _),
+    """SELECT r.*, r2.*
+      | FROM repositories r
+      | JOIN _references r2
+      |   ON r.id = r2.repository_id
+      | WHERE r2.name = 'refs/heads/master'
+    """.stripMargin -> (5, noop _)
+  )
+
+  def noop(df: DataFrame): Unit = {}
+
   "SQL queries" should {
-    Source.fromInputStream(getClass.getResourceAsStream("/queries.sql"))
-      .getLines
-      .foreach(sql => s"query: $sql" in {
-        val df = ss.sql(sql)
-        println(sql)
-        df.show
-        assert(df.count > 0)
-      })
+    tests.foreach(test => {
+      val (sql, (count, checker)) = test
+      sql in {
+        testQuery(sql, count, checker)
+      }
+    })
+  }
+
+  def testQuery(sql: String, count: Int, checker: DataFrameCheck = noop): Unit = {
+    val df = ss.sql(sql)
+    df.show
+    assert(df.count == count)
+    checker(df)
   }
 
   override protected def beforeAll(): Unit = {
