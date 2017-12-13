b5364e3ece2a24adb4ea553aaa5dc4128933ab76
Miguel Molina <miguel@erizocosmi.co>
2017-12-13 17:18:38 +0100

    fix saveMetadata test
    Signed-off-by: Miguel Molina <miguel@erizocosmi.co>

--- src/test/scala/tech/sourced/engine/EngineSpec.scala
+++ src/test/scala/tech/sourced/engine/EngineSpec.scala
@@ -33,10 +33,28 @@ class EngineSpec extends FlatSpec with Matchers with BaseSivaSpec with BaseSpark
 
     val properties = new Properties()
     properties.put("driver", "org.sqlite.JDBC")
-    Seq("repositories", "references", "commits", "tree_entries").foreach {
-      table =>
-        val count = getDataSource(table, ss).count()
-        ss.read.jdbc(s"jdbc:sqlite:$dbFile", s"engine_$table", properties)
+
+    val reposDf = getDataSource(RepositoriesTable, ss)
+    val refsDf = getDataSource(ReferencesTable, ss)
+    val repoHasCommitsDf = getDataSource(CommitsTable, ss)
+      .select("reference_name", "repository_id", "hash", "index")
+    val commitsDf = getDataSource(CommitsTable, ss)
+      .drop("index", "reference_name", "repository_id")
+      .distinct()
+    val treeEntriesDf = getDataSource(TreeEntriesTable, ss)
+      .drop("reference_name", "repository_id")
+      .distinct()
+
+    Seq(
+      (RepositoriesTable, reposDf),
+      (ReferencesTable, refsDf),
+      (RepositoryHasCommitsTable, repoHasCommitsDf),
+      (CommitsTable, commitsDf),
+      (TreeEntriesTable, treeEntriesDf)
+    ).foreach {
+      case (table, df) =>
+        val count = df.count()
+        ss.read.jdbc(s"jdbc:sqlite:$dbFile", Tables.prefix(table), properties)
           .count() should be(count)
     }
   }
