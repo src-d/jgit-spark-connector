d48418b6c1647c871ca5bb3b373e298c802be29f
Antonio Jesus Navarro Perez <antnavper@gmail.com>
2018-03-27 10:45:58 +0200

    Update version to 2.2.1 and fix readme
    Change readme to don't say that engine is compatible with other versions than 2.2.x
    
    Signed-off-by: Antonio Jesus Navarro Perez <antnavper@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
   lazy val scoverage = "org.scoverage" %% "scalac-scoverage-plugin" % "1.3.1"
-  lazy val sparkSql = "org.apache.spark" %% "spark-sql" % "2.2.0"
+  lazy val sparkSql = "org.apache.spark" %% "spark-sql" % "2.2.1"
   lazy val newerHadoopClient = "org.apache.hadoop" % "hadoop-client" % "2.7.2"
   lazy val fixNettyForGrpc = "io.netty" % "netty-all" % "4.1.11.Final"
   lazy val jgit = "org.eclipse.jgit" % "org.eclipse.jgit" % "4.9.0.201710071750-r"
