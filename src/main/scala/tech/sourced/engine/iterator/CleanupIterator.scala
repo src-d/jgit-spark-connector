bca93edbf2d1f395bf73a8668c5d2a98b5189a24
Carlos Martín <carlos.martin.sanchez@gmail.com>
2018-04-10 11:56:07 +0200

    Remove redundant class in CleanupIterator catch
    Signed-off-by: Carlos Martín <carlos.martin.sanchez@gmail.com>

--- src/main/scala/tech/sourced/engine/iterator/CleanupIterator.scala
+++ src/main/scala/tech/sourced/engine/iterator/CleanupIterator.scala
@@ -11,7 +11,7 @@ import org.apache.spark.{InterruptibleIterator, TaskContext}
   * @tparam T type of the rows in the iterator
   */
 class CleanupIterator[T](it: Iterator[T], cleanup: => Unit)
-  extends InterruptibleIterator[T](TaskContext.get(), it) {
+    extends InterruptibleIterator[T](TaskContext.get(), it) {
 
   /** @inheritdoc
     *
@@ -26,7 +26,7 @@ class CleanupIterator[T](it: Iterator[T], cleanup: => Unit)
       }
       hasNext
     } catch {
-      case e @ (_: Exception | _: RuntimeException) =>
+      case e: Exception =>
         it match {
           case it: ChainableIterator[_] =>
             throw new RepositoryException(it.repo, e)
