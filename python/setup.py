474bdbfbab4da90e2f1c755ea164f9408a7db2e7
Miguel Molina <miguel@erizocosmi.co>
2018-04-19 16:38:06 +0200

    python: require 2.2.x pyspark instead of >= 2.0.0
    Signed-off-by: Miguel Molina <miguel@erizocosmi.co>

--- python/setup.py
+++ python/setup.py
@@ -26,7 +26,7 @@ setup(
     url="https://github.com/src-d/engine/tree/master/python",
     packages=['sourced.engine'],
     namespace_packages=['sourced'],
-    install_requires=["pyspark>=2.0.0"],
+    install_requires=["pyspark==2.2.0.post0"],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
